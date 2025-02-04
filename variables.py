3546082ca9c051290c9596d97338b91e10e11bf6
Drzioner <drzioner@gmail.com>
2025-02-04 18:59:45 -0500

    feat(notebooks): ✨ Add introductory Python guides covering variables, operators, loops, and error handling

---
+++ variables.py
@@ -0,0 +1,9 @@
+nombre = "Juan"  # str (texto)
+edad = 30  # int (entero)
+precio = 19.99  # float (decimal)
+es_estudiante = True  # bool (booleano)
+
+print(type(nombre))
+print(type(edad))
+print(type(precio))
+print(type(es_estudiante))
